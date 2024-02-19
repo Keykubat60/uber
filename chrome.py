baafe04e44963c3794461aeaf0e60c25246b148e
alaaddin <alaaddinsagun60@gmail.com>
2024-02-19 14:06:24 +0100

    chrome.py commit

--- chrome.py
+++ chrome.py
@@ -4,16 +4,24 @@ from aiohttp import ClientSession
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException, WebDriverException, TimeoutException, StaleElementReferenceException
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
-import base64
 from geopy.geocoders import Nominatim
 from geopy.distance import geodesic
+import os
+from dotenv import load_dotenv
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
+# Laden der Umgebungsvariablen
+load_dotenv()
+
+# Umgebungsvariablen holen
+profile_path = os.getenv('USER_PROFILE_PATH')
+user_email = os.getenv('USER_EMAIL')
+user_password = os.getenv('USER_PASSWORD')
+headless_mode = os.getenv('HEADLESS_MODE') == 'True'
+webhook_adresse = os.getenv('WEBHOOK_ADRESSE')
 async def send_data_via_webhook(session, order_data):
-    webhook_url = 'https://webhook.site/20b9d417-0555-4675-9910-c51128883903'
+    webhook_url = webhook_adresse
     try:
         async with session.post(webhook_url, json=order_data) as response:
             content_type = response.headers.get('Content-Type', '')
@@ -25,6 +33,8 @@ async def send_data_via_webhook(session, order_data):
     except Exception as e:
         print(f"Webhook Fehler: {e}")
         return None
+
+
 async def get_distance(origin, destination):
     geolocator = Nominatim(user_agent="distance_calculator")
     try:
@@ -44,9 +54,11 @@ async def get_distance(origin, destination):
         print("Fehler bei der Berechnung der Entfernung:", e)
         return 0
 
+
 # Beispieladressen
 start_address = "Normannenstraße 28, 10365 Berlin, Deutschland"
 end_address = "Libauer Str. 12, 10245 Berlin, Deutschland"
+
 
 async def process_order(driver, order_element):
     try:
@@ -60,12 +72,11 @@ async def process_order(driver, order_element):
             "preis": price,
             "abholadresse": pickup_address,
             "zieladresse": destination_address,
-            "Entfernung" : f"{distance:.2f}",
+            "Entfernung": f"{distance:.2f}",
             "fahrer_name": driver_name,
             "fahrer_nachname": driver_surname,
 
         }
-
 
         async with ClientSession() as session:
 
@@ -79,6 +90,7 @@ async def process_order(driver, order_element):
     except Exception as e:
         print(f"")
 
+
 async def timerx(driver):
     await asyncio.sleep(6)
     # Warten, dann Button klicken
@@ -87,37 +99,98 @@ async def timerx(driver):
     ).click()'''
     print("Button geklickt")
 
-async def main():
-    profile_path = "C:\\Users\\alaad\\AppData\\Local\\Google\\Chrome\\User Data"
-    options = webdriver.ChromeOptions()
-    #options.add_argument("--headless")
-    # options.add_argument("--disable-headless-mode")
 
+async def main():
+    driver_path = r"C:\Users\alaad\OneDrive\Desktop\pythonProject\pythonProject\uber\chromedriver.exe"
+    options = webdriver.ChromeOptions()
+    if headless_mode:
+        options.add_argument("--headless")
+    # options.add_argument("--disable-headless-mode")
+    options.add_argument("--disable-blink-features=AutomationControlled")
+
+    # Exclude the collection of enable-automation switches 
+    options.add_argument("--log-level=3")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("--disable-extensions")
+
+    # Turn-off userAutomationExtension
+    options.add_experimental_option("useAutomationExtension", False)
     options.add_argument("--no-sandbox")
     options.add_argument(f"user-data-dir={profile_path}")
     options.add_argument('profile-directory=Default')
     options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
-
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=options)
+    driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     # driver.get("file:///C:/Users/alaad/Downloads/uber/1vsdispatch.uber.com.html")
     driver.get("https://vsdispatch.uber.com/")
     print(driver.get_cookies())
     await asyncio.sleep(1)
-    with open('Start.txt', 'w') as f:
-        f.write(driver.print_page())
     previous_order_count = 0
     while True:
         await asyncio.sleep(0.1)  # Polling-Intervall
         current_orders = driver.find_elements(By.CSS_SELECTOR, 'tr.MuiTableRow-root')
         if (len(current_orders)) == 0:
-            print("failed")
-            await asyncio.sleep(1)
-            with open('Error.txt', 'w') as f:
-                f.write(driver.print_page())
-            driver.get("https://vsdispatch.uber.com/")
-            await asyncio.sleep(1)
-            with open('Redirect.txt', 'w') as f:
-                f.write(driver.print_page())
+            try:
+                driver.get("https://vsdispatch.uber.com/")
+                WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located((By.ID, "PHONE_NUMBER_or_EMAIL_ADDRESS"))
+                )
+
+                # E-Mail-Adresse eingeben
+                email_input = driver.find_element(By.ID, "PHONE_NUMBER_or_EMAIL_ADDRESS")
+                await asyncio.sleep(1)
+                email_input.send_keys(user_email)
+                await asyncio.sleep(1)
+
+                # Auf den Weiter-Button klicken
+                continue_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.ID, "forward-button"))
+                )
+                continue_button.click()
+                await asyncio.sleep(1)
+
+                try:
+                    # Direktes Suchen nach dem Passwortfeld
+                    password_input = WebDriverWait(driver, 3).until(
+                        EC.presence_of_element_located((By.ID, "PASSWORD"))
+                    )
+                except:
+                    # Wenn das Passwortfeld nicht direkt gefunden wird, auf "Mehr Optionen" klicken
+                    more_options_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.ID, "alt-alternate-forms-option-modal"))
+                    )
+                    more_options_button.click()
+                    await asyncio.sleep(1)
+
+                    # Warten und klicken auf den Button "Passwort"
+                    password_button = WebDriverWait(driver, 10).until(
+                        EC.element_to_be_clickable((By.ID, "alt-more-options-modal-password"))
+                    )
+                    password_button.click()
+                    await asyncio.sleep(1)
+
+                    # Passwortfeld nach dem Klicken auf "Mehr Optionen"
+                    password_input = WebDriverWait(driver, 10).until(
+                        EC.presence_of_element_located((By.ID, "PASSWORD"))
+                    )
+
+                # Passwort eingeben
+                password_input.send_keys(user_password)
+                await asyncio.sleep(1)
+
+                # Auf den Weiter-Button klicken
+                final_continue_button = WebDriverWait(driver, 10).until(
+                    EC.element_to_be_clickable((By.ID, "forward-button"))
+                )
+                final_continue_button.click()
+
+                await asyncio.sleep(1)
+            except Exception as e:
+                print(e)
+                with open('2.txt', 'w') as f:
+                    f.write(driver.print_page())
+
+
         elif (len(current_orders)) != previous_order_count:
             for order in current_orders[previous_order_count:]:
                 asyncio.create_task(process_order(driver, order))
